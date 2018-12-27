44245c2fb90f7837a0c7bf7313cda6f880529ca7
Infant Frontender <newbornfrontender@gmail.com>
2018-12-27 21:12:16 +0300

    Добавил конфигурацию для babel

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        modules: false,
+
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
+};
